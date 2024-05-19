a46ee5224a7ff7fa47b91be80977435d82397ad8
Borja418 <borjapeiroten@gmail.com>
2024-05-19 22:44:32 +0200

    Ajuste Voxel FPFH

--- Base.py
+++ Base.py
@@ -18,9 +18,7 @@ def keypoints_to_spheres(keypoints, radius= 0.002):
 
     return spheres
 
-def fpfh(pcd):
-
-    pcd_sub = pcd.voxel_down_sample(CELL_SIZE) 
+def fpfh(pcd_sub):
 
     radius_normal = CELL_SIZE * 4
     pcd_sub.estimate_normals(o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn = 30))
@@ -28,7 +26,7 @@ def fpfh(pcd):
     radius_feature = CELL_SIZE * 6
     pcd_fpfh = o3d.pipelines.registration.compute_fpfh_feature(pcd_sub, o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn = 300))
 
-    return pcd_sub, pcd_fpfh
+    return pcd_fpfh
 
 
 def extraer_keypoints(pcd):
@@ -71,9 +69,10 @@ def correspondencias(fpfh_escena, fpfh_objeto, mutua = True):
 
 def main():
 
-    pcd_objeto = o3d.io.read_point_cloud("clouds/objects/s0_piggybank_corr.pcd")
+    pcd_objeto = o3d.io.read_point_cloud("clouds/objects/s0_plc_corr.pcd")
     pcd_escena = o3d.io.read_point_cloud("clouds/scenes/snap_0point.pcd")
 
+    inicio = time.time()
     plane_model, inliers = pcd_escena.segment_plane(distance_threshold = 0.05, ransac_n  = 3, num_iterations = 200)
     outlier_cloud = pcd_escena.select_by_index(inliers, invert=True)
 
@@ -83,9 +82,12 @@ def main():
     plane_model, inliers3 = outlier_cloud.segment_plane(distance_threshold = 0.02, ransac_n  = 3, num_iterations = 200)
     outlier_cloud = outlier_cloud.select_by_index(inliers3, invert=True)
 
+    pcd_sub_escena = outlier_cloud.voxel_down_sample(CELL_SIZE) 
+    pcd_sub_objeto = pcd_objeto.voxel_down_sample(CELL_SIZE) 
+
     inicio_keypoints = time.time()
-    pcd_sub_escena, pcd_fpfh_escena = fpfh(outlier_cloud)
-    pcd_sub_objeto, pcd_fpfh_objeto = fpfh(pcd_objeto)
+    pcd_fpfh_escena = fpfh(pcd_sub_escena)
+    pcd_fpfh_objeto = fpfh(pcd_sub_objeto)
 
     keypoints_escena = extraer_keypoints(pcd_sub_escena)
     keypoints_objeto = extraer_keypoints(pcd_sub_objeto)
@@ -132,12 +134,9 @@ def main():
         o3d.pipelines.registration.TransformationEstimationPointToPoint(False), 4,
         [o3d.pipelines.registration.CorrespondenceCheckerBasedOnDistance(distance_threshold)],
         o3d.pipelines.registration.RANSACConvergenceCriteria(5000, 0.999))
-        #o3d.pipelines.registration.RANSACConvergenceCriteria(20000, 0.999))
     fin_RANSAC = time.time()
 
     evaluation = o3d.pipelines.registration.evaluate_registration(pcd_sub_objeto, pcd_sub_escena, THRESHOLD, result.transformation)
-
-
 
     inicio_ICP = time.time()
     result_icp = o3d.pipelines.registration.registration_icp(pcd_sub_objeto, pcd_sub_escena, THRESHOLD, result.transformation, o3d.pipelines.registration.TransformationEstimationPointToPoint(), o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=30))
@@ -147,6 +146,7 @@ def main():
     print(f"(Correspondencias - {round(fin_correspondencias-inicio_correspondecias,4)})")
     print(f"(RANSAC - {round(fin_RANSAC-inicio_RANSAC,4)}) Fitness:{evaluation.fitness} RMSE:{evaluation.inlier_rmse}")
     print(f"(ICP - {round(fin_ICP-inicio_ICP,4)}) Fitness:{result_icp.fitness} RMSE:{result_icp.inlier_rmse}")
+    print(f"Tiempo Total: {round(fin_ICP-inicio,4)}")
 
     pcd_objeto.transform(result_icp.transformation)
     pcd_objeto.paint_uniform_color([1, 0, 0])
